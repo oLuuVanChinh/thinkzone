fa7a401e5d6e0083209b82e0f91a16d7cee76d0b
oLuuVanChinh <chinhlv91@gmail.com>
2021-03-21 23:05:35 +0700

    Add page home, contact, partner

--- public/assets/tznew/js/theme.js
+++ public/assets/tznew/js/theme.js
@@ -6,29 +6,56 @@
         windowHeight = $(window).height(),
         windowWidth = $(window).width();
 
+    window.getScroll = function(){
+      var scroll = $(window).scrollTop();
+      if(scroll > 1){
+        $('.header-container').css("background-color", '#fff').addClass('is-sticky');
+      } else {
+        $('.header-container').css("background-color", 'transparent').removeClass('is-sticky');
+      }
+      if($('.header-container').hasClass('header-transparent')){
+        if(scroll > 1){
+            $('.logo').find('img').attr("src", "public/assets/tznew/css/images/logo-dark.png");
+        } else {
+            $('.logo').find('img').attr("src", "public/assets/tznew/css/images/logo-white.png");
+        }
+      }
+    }
+
     var dgtTheme = {
         init: function () {
           // Slick Slider
           var slick = $('#section-partner'),
               slickSlider = $('#slider-top'),
               sliderPostCenter = $('#slider-post-center');
-          slick.slick({
-            slidesToShow: 5,
-            infinite: true,
-          });
-          slickSlider.slick({
-            centerMode: true,
-            slidesToShow: 1,
-            infinite: true,
-            centerPadding: '220px',
-          });
-          sliderPostCenter.slick({
-            centerMode: true,
-            slidesToShow: 3,
-            infinite: true,
-            centerPadding: '0',
-            dots: true,
-          });
+          if(slick.length){
+            slick.slick({
+              slidesToShow: 5,
+              infinite: true,
+            });
+          }
+          if(slickSlider.length){
+            slickSlider.slick({
+              centerMode: true,
+              slidesToShow: 1,
+              infinite: true,
+              arrows: false,
+              autoplay: true,
+              centerPadding: '220px',
+            });
+            slickSlider.on('init', function(slick){
+              $(this).find('.slick-current').next().addClass('works');
+            });
+          }
+          if(sliderPostCenter.length){
+            sliderPostCenter.slick({
+              centerMode: true,
+              slidesToShow: 3,
+              infinite: true,
+              centerPadding: '0',
+              dots: true,
+            });
+          }
 
           // Video Player
           var videoPlay = $('button.btn-play')
@@ -44,13 +71,9 @@
 
     $(document).ready(function () {
         dgtTheme.init();
+        getScroll();
     });
     $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        if(scroll > 1){
-          $('.header-container').css("background-color", '#fff');
-        } else {
-          $('.header-container').css("background-color", 'transparent');
-        }
+      getScroll();
     });
 })(window.jQuery);
